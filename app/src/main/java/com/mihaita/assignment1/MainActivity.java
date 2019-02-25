eaa0ed115348472618e43f0312d2019ca1529f03
Mihai <lacatus.mihaita@gmail.com>
2019-02-24 20:19:58 -0500

    Initial commit

---
+++ app/src/main/java/com/mihaita/assignment1/MainActivity.java
@@ -0,0 +1,37 @@
+package com.mihaita.assignment1;
+
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle t;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //modify action bar
+        dl = (DrawerLayout) findViewById(R.id.drawer_layout);
+        t = new ActionBarDrawerToggle
+                (this, dl, R.string.nav_open, R.string.nav_close);
+        dl.addDrawerListener(t);
+        t.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+    }
+
+    //make menu clickable
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (t.onOptionsItemSelected(item))
+            return true;
+        return super.onOptionsItemSelected(item);
+    }
+}
