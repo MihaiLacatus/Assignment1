fe357559d99c2bdd3a63ef041f56aff64d46fecb
Mihai <lacatus.mihaita@gmail.com>
2019-02-24 23:45:04 -0500

    HistoryActivity ready

--- app/src/main/java/com/mihaita/assignment1/MainActivity.java
+++ app/src/main/java/com/mihaita/assignment1/MainActivity.java
@@ -8,6 +8,8 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -29,7 +31,17 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         NavigationView nv = findViewById(R.id.nav_view);
         nv.setNavigationItemSelectedListener(this);
 
-
+        Button b0 = findViewById(R.id.btn_zero), b1 = findViewById(R.id.btn_1),
+                b2 = findViewById(R.id.btn_2), b3 = findViewById(R.id.btn_3),
+                b4 = findViewById(R.id.btn_4), b5 = findViewById(R.id.btn_5),
+                b6 = findViewById(R.id.btn_6), b7 = findViewById(R.id.btn_7),
+                b8 = findViewById(R.id.btn_8), b9 = findViewById(R.id.btn_9),
+                bDot = findViewById(R.id.btn_dot), bEqual = findViewById(R.id.btn_equal),
+                bAdd = findViewById(R.id.btn_add), bSubstract = findViewById(R.id.btn_sub),
+                bMultiply = findViewById(R.id.btn_multiply), bDivide = findViewById(R.id.btn_divide),
+                bDel = findViewById(R.id.btn_del), bPercentage = findViewById(R.id.btn_percent),
+                bPozNeg = findViewById(R.id.btn_sign), bClear = findViewById(R.id.btn_clear);
+        TextView tvCalculations = findViewById(R.id.textView_calculation);
 
 
     }
@@ -43,11 +55,10 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     }
 
     @Override
-    public boolean onNavigationItemSelected(MenuItem item)
-    {
-        if (item.getItemId() == R.id.nav_about){
+    public boolean onNavigationItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.nav_about) {
 
-            Intent i = new Intent (getApplicationContext(),HistoryActivity.class);
+            Intent i = new Intent(getApplicationContext(), HistoryActivity.class);
             startActivity(i);
         }
 
