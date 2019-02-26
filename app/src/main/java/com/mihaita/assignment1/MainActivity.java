4593d2d8b80d8656a9f3b94fd592ab61a0b68521
Mihai <lacatus.mihaita@gmail.com>
2019-02-25 23:13:58 -0500

    calculations almost done

--- app/src/main/java/com/mihaita/assignment1/MainActivity.java
+++ app/src/main/java/com/mihaita/assignment1/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -15,6 +16,84 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     private DrawerLayout dl;
     private ActionBarDrawerToggle t;
+
+    double numberOne, numberTwo, total;
+
+    boolean addBtnPressed, subtractBtnPressed, divideBtnPressed, multiplyBtnPressed, equalBtnPressed,
+            anyMathBtnPressed, firstNumberPressed, secondNumberPressed, calculationFinished;
+
+    String history = "";
+
+    public String getHistory() {
+        return history;
+    }
+
+    public void pressButton(boolean mathBtnPressed, String number) {
+        TextView result = findViewById(R.id.textView_calculation);
+
+        if (mathBtnPressed == true || equalBtnPressed == true) {
+            result.setText("");
+        }
+        // sa incercam un else aici
+
+        result.setText((result.getText().toString().equals("0")) ? number : result.getText() + number);
+
+        anyMathBtnPressed = false;
+        equalBtnPressed = false;
+        firstNumberPressed = true;
+
+        history = history + number;
+    }
+
+    public void takeInputForMath() {
+        secondNumberPressed = true;
+
+        TextView result = findViewById(R.id.textView_calculation);
+        //make one line with if first condition && second condition
+        if (!result.getText().toString().isEmpty()) {
+            anyMathBtnPressed = true;
+
+            if (secondNumberPressed == true) {
+                calculate();
+            }
+
+            numberOne = Double.parseDouble(result.getText().toString());
+        }
+
+        addBtnPressed = false;
+        subtractBtnPressed = false;
+        divideBtnPressed = false;
+        multiplyBtnPressed = false;
+
+    }
+
+    public void calculate() {
+        total = 0;
+
+        TextView result = findViewById(R.id.textView_calculation);
+
+        if (secondNumberPressed == true || equalBtnPressed == true || calculationFinished == true) {
+            //aici nu e nevoie de nr 3 poate fi direct nr 1
+
+            double number3 = Double.parseDouble(result.getText().toString());
+            numberOne = number3;
+        } else { numberTwo = Double.parseDouble(result.getText().toString()); }
+
+        if (addBtnPressed==true) {
+            total = numberOne + numberTwo;
+        } else if (subtractBtnPressed==true){
+            total = numberOne - numberTwo;
+        } else if (divideBtnPressed==true && numberTwo!=0){
+            total = numberOne / numberTwo;
+        } else if (multiplyBtnPressed==true){
+            total = numberOne * numberTwo;
+        } else { total = numberTwo;}
+
+        secondNumberPressed = true;
+        calculationFinished = true;
+
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,11 +116,175 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 b6 = findViewById(R.id.btn_6), b7 = findViewById(R.id.btn_7),
                 b8 = findViewById(R.id.btn_8), b9 = findViewById(R.id.btn_9),
                 bDot = findViewById(R.id.btn_dot), bEqual = findViewById(R.id.btn_equal),
-                bAdd = findViewById(R.id.btn_add), bSubstract = findViewById(R.id.btn_sub),
+                bAdd = findViewById(R.id.btn_add), bSubtract = findViewById(R.id.btn_sub),
                 bMultiply = findViewById(R.id.btn_multiply), bDivide = findViewById(R.id.btn_divide),
                 bDel = findViewById(R.id.btn_del), bPercentage = findViewById(R.id.btn_percent),
                 bPozNeg = findViewById(R.id.btn_sign), bClear = findViewById(R.id.btn_clear);
-        TextView tvCalculations = findViewById(R.id.textView_calculation);
+        TextView result = findViewById(R.id.textView_calculation);
+
+
+        //missing some cases for zero number
+        b0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pressButton(anyMathBtnPressed,"0");
+            }
+        });
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pressButton(anyMathBtnPressed,"1");
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pressButton(anyMathBtnPressed,"2");
+            }
+        });
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pressButton(anyMathBtnPressed,"3");
+            }
+        });
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pressButton(anyMathBtnPressed,"4");
+            }
+        });
+        b5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pressButton(anyMathBtnPressed,"5");
+            }
+        });
+        b6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pressButton(anyMathBtnPressed,"6");
+            }
+        });
+        b7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pressButton(anyMathBtnPressed,"7");
+            }
+        });
+        b8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pressButton(anyMathBtnPressed,"8");
+            }
+        });
+        b9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pressButton(anyMathBtnPressed,"9");
+            }
+        });
+        bDot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView result = findViewById(R.id.textView_calculation);
+                result.setText(result.getText().toString() + ".");
+                history = history + ".";
+            }
+        });
+        bEqual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculate();
+                equalBtnPressed = true;
+
+                secondNumberPressed=false;
+                addBtnPressed = false;
+                subtractBtnPressed = false;
+                divideBtnPressed = false;
+                multiplyBtnPressed = false;
+                anyMathBtnPressed = false;
+
+                TextView result = findViewById(R.id.textView_calculation);
+                //putin diferit aici
+                history = history + "=" + result.getText().toString() +"\n";
+
+            }
+        });
+        bAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                takeInputForMath();
+                addBtnPressed = true;
+                history = history + "+";
+            }
+        });
+        bSubtract.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                takeInputForMath();
+                subtractBtnPressed = true;
+                history = history + "-";
+            }
+        });
+        bMultiply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                takeInputForMath();
+                multiplyBtnPressed = true;
+                history= history + "*";
+            }
+        });
+        bDivide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                takeInputForMath();
+                divideBtnPressed = true;
+                history = history + "/";
+            }
+        });
+        bDel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView result = findViewById(R.id.textView_calculation);
+                result.setText("");
+                history = history + "DEL";
+            }
+        });
+        bPercentage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView result = findViewById(R.id.textView_calculation);
+                result.setText(String.valueOf(Double.parseDouble(result.getText().toString())/100));
+                history = history + "%";
+            }
+        });
+        bPozNeg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView result = findViewById(R.id.textView_calculation);
+                result.setText(String.valueOf(Double.parseDouble(result.getText().toString()) * -1));
+                history = history + "+/-";
+            }
+        });
+        bClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView result = findViewById(R.id.textView_calculation);
+                result.setText("0");
+                numberOne = 0;
+                numberTwo = 0;
+                anyMathBtnPressed = false;
+                firstNumberPressed = false;
+                addBtnPressed = false;
+                subtractBtnPressed = false;
+                multiplyBtnPressed = false;
+                divideBtnPressed = false;
+                calculationFinished = false;
+                history = history + "C\n";
+            }
+        });
+
 
 
     }
